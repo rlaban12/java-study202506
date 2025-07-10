b2fe23cb259b396df02325c169dd6566d3a4eda0
rlaban12 <114490382+rlaban12@users.noreply.github.com>
2025-07-10 16:14:58 +0900

    20250710_1614 commit

--- src/chap2_8/pratice/q1/Transaction.java
+++ src/chap2_8/pratice/q1/Transaction.java
@@ -32,4 +32,12 @@ public class Transaction {
                 '}';
     }
 
+    public String toStringPrettier() {
+        return "{" +
+                "trader: " + trader +
+                ", year: " + year +
+                ", value: " + value +
+                '}';
+    }
+
 }
